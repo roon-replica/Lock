cf10618ac5c8a1e6a359251e4b909696e0f0320d
roon <roon.think@navercorp.com>
2025-02-01 19:36:49 +0900

    initial commit

---
+++ src/test/java/roon/practice/lock/lock/LockApplicationTests.java
@@ -0,0 +1,13 @@
+package roon.practice.lock.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
